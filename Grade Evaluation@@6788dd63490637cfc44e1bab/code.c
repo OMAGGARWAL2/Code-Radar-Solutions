0132ce17ef7e63d91e43bc45a00dac8f611745f5
OMAGGARWAL2 <197039954+OMAGGARWAL2@users.noreply.github.com>
2025-02-12 18:42:17 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -10,7 +10,7 @@ int main(){
         printf("Average");
     } else if (ch == 'D'){
         printf("Below Average");
-    }else if (ch == 'E'){
+    }else if (ch == 'F'){
         printf("Fail");
     } else {
         printf("Invalid grade")
