8ea738cf195f1366bba822bad41cb2f2c7cdbc62
OMAGGARWAL2 <197039954+OMAGGARWAL2@users.noreply.github.com>
2025-02-12 17:59:19 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,13 +5,13 @@ int main(){
     char op;
     scanf("%d %d  %c", &a, &b, &op);
     if (char op == '+'){
-        printf(a + b);
+        printf("%d\n", a + b);
     } else if (char op == '-'){
-        printf(a - b);   
+        printf("%d\n", a - b);   
     }else if (char op == '*'){
-        printf(a * b);   
+        printf("%d\n", a * b);   
     }else if (char op == '/'){
-        printf(a / b);   
+        printf("%d\n", a / b);   
     }
     return 0;
 }
